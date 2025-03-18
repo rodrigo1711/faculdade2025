4a6b6ca6ab0b21251996d7e51333f762daadfa2f
rodrigopacheco <l.rmacedo17@gmail.com>
2025-03-18 12:43:13 -0300

    Create Main.java

---
+++ ark.soft/aula1/aula2/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		
+		Narguile narguile1 = new Narguile("Ceramica","Coco","Love 66",10);
+		
+		//narguile1.setQtdCarvao(5);
+		//narguile1.setRosh("Ceramica");
+		
+		System.out.println(narguile1.getQtdCarvao());
+		System.out.println(narguile1.getRosh());
+		System.out.println(narguile1.getEssencia());
+		System.out.println(narguile1.getTipoCarvao());
+		
+	}
+}
